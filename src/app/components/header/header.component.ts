7bc2258f48d66ca2c24e69cdb2eeffc5dcbab15a
IneMentenPXL <Ine.Menten@student.pxl.be>
2022-04-27 20:14:51 +0200

    Add gradient

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  @Input() title: string = '';
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let child = this.activatedRoute.firstChild;
+          while (child) {
+            if (child.firstChild) {
+              child = child.firstChild;
+            } else if (child.snapshot.data && child.snapshot.data['title']) {
+              return child.snapshot.data['title'];
+            } else {
+              return null;
+            }
+          }
+          return null;
+        })
+      )
+      .subscribe((data: any) => {
+        if (data) {
+          this.title = data;
+        }
+      });
+  }
+
+  ngOnInit(): void {}
+}
